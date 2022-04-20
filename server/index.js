e6fdc433d40e58b4692eda167fdd63bd7bd3cefd
Scorpion197 <kamelgaouaoui197@gmail.com>
2022-04-20 11:18:39 +0100

    finished index.js

---
+++ server/index.js
@@ -0,0 +1,20 @@
+require("dotenv").config({ path: ".env" });
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const pusher = require("pusher");
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+app.use((req, rest, next) => {
+  rest.header("Access-Control-Allow-Origin", "*");
+  next();
+});
+app.set("port", process.env.PORT || 5000);
+const server = app.listen(() => {
+  console.log(`Express running on: ${app.get("port")}`);
+});
