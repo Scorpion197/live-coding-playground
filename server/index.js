b31cc6b564a5a24687c78d1df7eef2a79fd6fed6
Scorpion197 <kamelgaouaoui197@gmail.com>
2022-04-20 18:14:17 +0100

    updated server index.js

--- server/index.js
+++ server/index.js
@@ -15,6 +15,13 @@ app.use((req, rest, next) => {
   next();
 });
 
+app.post("/update-editor", (req, res) => {
+  pusher.trigger("editor", "code-update", {
+    ...req.body,
+  });
+
+  res.status(200).send("OK");
+});
 app.set("port", process.env.PORT || 5000);
 const server = app.listen(() => {
   console.log(`Express running on: ${app.get("port")}`);
